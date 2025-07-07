3e5ca9a14c57188e481b1c2e4d0aef1aa2093adb
Tutorial Hell <booleanorama@gmail.com>
2025-07-07 07:29:42 -0400

    list tickets page

--- src/actions/ticket.actions.ts
+++ src/actions/ticket.actions.ts
@@ -1,35 +1,73 @@
 "use server";
-import * as Sentry from "@sentry/nextjs";
+// import * as Sentry from "@sentry/nextjs";
 import { prisma } from "@/db/prisma";
 import { revalidatePath } from "next/cache";
+import { logEvent } from "@/utils/sentry";
 
 export async function createTicket(
   prevState: { success: boolean; message: string },
   formData: FormData
 ): Promise<{ success: boolean; message: string }> {
   try {
-    throw new Error("Simulated Prisma Error For Testing");
+    // throw new Error("Simulated Prisma Error For Testing");
     const subject = formData.get("subject") as string;
     const description = formData.get("description") as string;
     const priority = formData.get("priority") as string;
 
     // console.log(subject, description, priority);
     if (!subject || !description || !priority) {
-      Sentry.captureMessage(
-        "Validation Error: Missing ticket fields",
+      logEvent(
+        "Validation Error: Missing data fields",
+        "ticket",
+        { subject, description, priority },
         "warning"
       );
       return { success: false, message: "All fields are required" };
     }
 
+    const ticket = await prisma.ticket.create({
+      data: { subject, description, priority },
+    });
+
+    logEvent(
+      `Ticket created successfully ${ticket.id}`,
+      "ticket",
+      { ticketId: ticket.id },
+      "info"
+    );
+
+    revalidatePath("/tickets/");
+
     return { success: true, message: "Ticket created successfully" };
   } catch (error) {
-    Sentry.captureException(error as Error, {
-      extra: { formData: Object.fromEntries(formData.entries()) },
-    });
+    logEvent(
+      "An error occurred while creating the ticket",
+      "ticket",
+      { formData: Object.fromEntries(formData.entries()) },
+      "error",
+      error
+    );
     return {
       success: false,
       message: "An error occurred while creating the ticket",
     };
+  }
+}
+
+export async function getTickets() {
+  try {
+    const tickets = await prisma.ticket.findMany({
+      orderBy: { createdAt: "desc" },
+    });
+    logEvent(
+      "Fetched ticket list",
+      "ticket",
+      { count: tickets.length },
+      "info"
+    );
+    return tickets;
+  } catch (error) {
+    logEvent("Error fetching tickets", "tickets", {}, "error", error);
+    return [];
   }
 }
